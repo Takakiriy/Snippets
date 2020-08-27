65ded8c89f12b31bb70cabb4160b9e6a30ee1bd8
Takakiri <takakiriy@gmail.com>
2020-08-28 00:18:21 +0900

    install/cypress >> Added cypress fiddle

---
+++ for-bash/installer/cypress/base/cypress/integration/cypress-fiddle.js
@@ -0,0 +1,55 @@
+﻿/// <reference types="@cypress/fiddle" />
+import * as cmd from "../my-cmd.js"
+import * as lib from "../my-lib.js"
+
+const helloTest = {
+  html: `
+    <div>Hello</div>
+  `,
+  test: `
+	function  main() {
+		cy.get('div').then( (element) => {
+			log(element)         // <div>
+			log(element.text())  // Hello
+		})
+	}
+
+	// log
+	function  log( label,  value ) {
+		if (typeof value === 'object') {
+			expect( label ).to.not.eq( lib.recursiveAssign( {}, value ) )
+		} else if ( label === value ) {
+			expect( label ).to.eq( value )
+		} else {
+			expect( label ).to.not.eq( value )
+		}
+	}
+
+	// recursiveAssign is nested Object.assign
+	function  recursiveAssign(a, b) {
+		const bIsObject = (Object(b) === b);
+		if (!bIsObject) {
+			return b;
+		}
+
+		const aIsObject = (Object(a) === a);
+		if (!aIsObject) {
+			if (b instanceof Array) {
+				a = [];
+			} else {
+				a = {};
+			}
+		}
+		for (const key of Object.keys(b)) {
+			a[key] = recursiveAssign(a[key], b[key]);
+		}
+		return a;
+	}
+
+	main()
+`
+}
+
+it('tests hello', () => {
+  cy.runExample(helloTest)
+})
--- for-bash/installer/cypress/login/cypress/support/index.js
+++ for-bash/installer/cypress/login/cypress/support/index.js
@@ -16,5 +16,8 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+// adds "cy.runExample()" command
+import '@cypress/fiddle'
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
